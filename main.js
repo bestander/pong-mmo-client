2394ac32a77cefa869cc8f034a550e7839a176da
Konstantin Raev <bestander@gmail.com>
2012-10-22 16:42:11 +1300

    returned main.js back

--- main.js
+++ main.js
@@ -1,14 +1,97 @@
-/* An example how this package can be used in a RequireJS application
- * The script just adds a DIV element to body and calls init() on the library module.
- *
+/*
+ * Author: Svetlana Raeva (svetlana.myth@gmail.com)
  * Author: Konstantin Raev (bestander@gmail.com)
  * Released under the MIT license
- * */
+*/
 define(function (require) {
-  var pong = require('../../../../main')
-    , $ = require('jquery')
-    ;
-  $("body").add("<div id='canvasElemId' style='with:400px; height:400px'/>");
 
-  pong.init('canvasElemId');
+  var $ = require('jquery')
+    , logic = require('./lib/gameLogic')
+    , renderer = require('./lib/gameRenderer')
+    ;
+
+  var game = new logic.game(function (position) {
+    //console.log(position);
+  });
+
+  function drawRectangle(myRect, context) {
+    context.beginPath();
+    context.rect(myRect.x, myRect.y, myRect.width, myRect.height);
+    context.fillStyle = "#FFFFFF";
+    context.fill();
+    context.stroke();
+  }
+
+  function animate(myRect, canvas, context, startTime) {
+    // update
+    var time = (new Date()).getTime() - startTime;
+
+    var linearSpeed = 100;
+    // pixels / second
+    var newX = linearSpeed * time / 1000;
+
+    if (newX < canvas.width - myRect.width) {
+      myRect.x = newX;
+    }
+
+    // clear
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, 600, 600);
+
+    //alert("x = " + myRect.x + " y=" + myRect.y);
+
+    drawRectangle(myRect, context);
+
+    // request new frame
+    requestAnimFrame(function () {
+      animate(myRect, canvas, context, startTime);
+    });
+  }
+
+
+  var oldInit = function () {
+    window.requestAnimFrame = (function (callback) {
+      return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+        function (callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+    })();
+
+    window.onload = function () {
+      var canvas = document.getElementById("myCanvas");
+      var context = canvas.getContext("2d");
+      context.fillStyle = "#000000";
+      context.fillRect(0, 0, 600, 600);
+
+      var myRect = {
+        x:10,
+        y:10,
+        width:20,
+        height:20
+      };
+
+      drawRectangle(myRect, context);
+
+      setTimeout(function () {
+        var startTime = (new Date()).getTime();
+        animate(myRect, canvas, context, startTime);
+      }, 1000);
+    };
+  };
+
+  return {
+    init: function(divId){
+      renderer.init(divId)
+    },
+
+    startGame:function () {
+      alert("start game");
+    },
+
+    movePaddle:function () {
+      alert("movePaddle");
+    }
+
+  }
 });
