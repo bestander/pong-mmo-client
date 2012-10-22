ed1152a1ec4ffcfb048fd26e27f03a377da6c538
Konstantin Raev <bestander@gmail.com>
2012-10-22 19:31:31 +1300

    I guess I fixed the dependencies problem

--- volo-create-example-bootstrap.sh
+++
@@ -1,7 +0,0 @@
-# run this script to create a sample html file that will test the library standalone
-
-volo create example # creates a folder with require.js dependency and an empty index.html
-cd example
-volo install jquery # downloads jquery from repository
-cp -f ../amd-example-bootstrap.js ./www/js/app/main.js # replace empty main module with ours that adds canvas to index.html and starts up the application
-
