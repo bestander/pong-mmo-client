b006d5acaa82b983fd6f5c2282b34810f5ed6b4c
Konstantin Raev <bestander@gmail.com>
2013-01-04 23:49:31 +1300

    some progress in rendering, next - finishing up game master

--- src/PongGameRenderer.js
+++ src/PongGameRenderer.js
@@ -17,89 +17,85 @@ var Interpolator = require('animation-smoother');
  */
 function PongGameRenderer(graphicsContainerId) {
   this._graphicsContainerId = graphicsContainerId;
+  this._scene = null;
+  this._ballObject = null;
 }
 
 module.exports = PongGameRenderer;
 
 /**
- * change rendering scene: intro, lobby, match
+ * display scene: intro, lobby, match
  */
 PongGameRenderer.prototype.showScene = function () {
+  var boundingBox;
+  var boundingBoxConfig;
+  var pointLight;
+  var that = this;
   var animate;
-  var sphere;
-  var sphereMaterial;
-  var scene;
   var renderer;
   var camera;
-  var radius, segments, rings;
   var container = document.getElementById(this._graphicsContainerId);
-  var width = container.style.width.replace("px", "");
-  var height = container.style.height.replace("px", "");
-
-  // set some camera attributes
   var VIEW_ANGLE, ASPECT, NEAR, FAR;
+
   VIEW_ANGLE = 45;
-  ASPECT = width / height;
   NEAR = 0.1;
   FAR = 10000;
+  this._fieldWidth = container.style.width.replace("px", "");
+  this._filedHeight = container.style.height.replace("px", "");
+  ASPECT = this._fieldWidth / this._filedHeight;
 
   // create a WebGL renderer, camera
   // and a scene
   renderer = new THREE.WebGLRenderer();
-  scene = new THREE.Scene();
+  // start the renderer
+  renderer.setSize(this._fieldWidth, this._filedHeight);
+
   camera = new THREE.PerspectiveCamera(
       VIEW_ANGLE,
       ASPECT,
       NEAR,
       FAR);
-
-  scene.add(camera);
-
   // the camera starts at 0,0,0
   // so pull it back
-  camera.position.z = 300;
+  camera.position.z = 600;
 
-  // start the renderer
-  renderer.setSize(width, height);
+  this._scene = new THREE.Scene();
+  this._scene.add(camera);
+
+  // create a point light
+  pointLight = new THREE.PointLight(0xFFFFFF);
+  pointLight.position.x = 10;
+  pointLight.position.y = 50;
+  pointLight.position.z = 130;
+
+//  this._scene.add(pointLight);
+
+  // outer bounds
+  boundingBoxConfig = {
+    width: this._fieldWidth,
+    height: this._filedHeight,
+    depth: 1200,
+    splitX: 6,
+    splitY: 6,
+    splitZ: 20
+  };
+  boundingBox = new THREE.Mesh(
+    new THREE.CubeGeometry(
+      boundingBoxConfig.width, boundingBoxConfig.height, boundingBoxConfig.depth,
+      boundingBoxConfig.splitX, boundingBoxConfig.splitY, boundingBoxConfig.splitZ),
+    new THREE.MeshBasicMaterial( { color: 0xffaa00, wireframe: true } )
+  );
+  this._scene.add(boundingBox);
+
 
   // attach the render-supplied DOM element
   container.appendChild(renderer.domElement);
-
-  // set up the sphere vars
-  radius = 10;
-  segments = 16;
-  rings = 16;
-
-  // create the sphere's material
-  sphereMaterial = new THREE.MeshLambertMaterial(
-  {
-    color:0xCC0000
-  });
-
-  // create a new mesh with
-  // sphere geometry - we will cover
-  // the sphereMaterial next!
-  sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(
-      radius,
-      segments,
-      rings),
-    sphereMaterial);
-
-  // add the sphere to the scene
-  scene.add(sphere);
-
-  this._ballPositions = new Interpolator({x: 50, y: 50});
-  this._ballPositions.onCoordinateRequest(function () {
-    sphere.position.x = this.x;
-    sphere.position.y = this.y;
-  });
 
   animate = function() {
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame(animate);
     Interpolator.updateAll();
-    renderer.render(scene, camera);
+    renderer.render(that._scene, camera);
   };
   animate();
 };
@@ -108,26 +104,88 @@ PongGameRenderer.prototype.showScene = function () {
  * render changes to game: objects changed positions, player scored
  */
 PongGameRenderer.prototype.renderGameUpdate = function (objects) {
-  this._ballPositions.scheduleNext({
-    x: objects.BALL.position.x,
-    y: objects.BALL.position.y
-  }, objects.delay);
+  if(objects.BALL && this._ballObject){
+    this._ballObject.interpolated.scheduleNext({
+      x: objects.BALL.position.x,
+      y: objects.BALL.position.y
+    }, objects.delay);
+  }
+  if(objects.leftPlayer && this._leftPlayer){
+    this._leftPlayer.interpolated.scheduleNext({
+      y: objects.leftPlayer.position.y
+    }, objects.delay);
+  }
+  if(objects.rightPlayer && this._rightPlayer){
+    this._rightPlayer.interpolated.scheduleNext({
+      y: objects.rightPlayer.position.y
+    }, objects.delay);
+  }
 };
 
-/**
- * add object to current scene
- */
-PongGameRenderer.prototype.addObject = function () {
-
+PongGameRenderer.prototype.addBall = function (position) {
+  if(!this._ballObject){
+    // create the sphere's material
+    this._ballObject = new THREE.Mesh(
+      new THREE.SphereGeometry(
+        10,
+        16,
+        16),
+      new THREE.MeshLambertMaterial(
+        {
+          color:0xCC0000
+        })
+    );
+    this._defineInterpolator(this._ballObject, position);
+    this._scene.add(this._ballObject);
+  }
 };
 
-/**
- * remove object from scene
- * @param id
- */
-PongGameRenderer.prototype.removeObject = function (id) {
-
+PongGameRenderer.prototype.addPlayer = function (params) {
+  if(params.type === 'left' && !this._leftPlayer){
+    // create the sphere's material
+    this._leftPlayer = this._definePaddle();
+    this._defineInterpolator(this._leftPlayer, {x: -this._fieldWidth / 2, y: 100});
+    this._scene.add(this._leftPlayer);
+  }
+  if(params.type === 'right' && !this._rightPlayer){
+    // create the sphere's material
+    this._rightPlayer = this._definePaddle();
+    this._defineInterpolator(this._rightPlayer, {x: this._fieldWidth / 2, y: 100});
+    this._scene.add(this._rightPlayer);
+  }
 };
 
+PongGameRenderer.prototype.removePlayer = function (type) {
+  if(type === 'left') {
+    this._scene.remove(this._leftPlayer);
+    this._leftPlayer = null;
+  }
+  if(type === 'right') {
+    this._scene.remove(this._rightPlayer);
+    this._rightPlayer = null;
+  }
+};
 
+PongGameRenderer.prototype._definePaddle = function () {
+  return new THREE.Mesh(
+    new THREE.CubeGeometry(
+      5,
+      60,
+      1),
+    new THREE.MeshLambertMaterial(
+      {
+        color:0xCC0000
+      })
+  );
+};
+
+PongGameRenderer.prototype._defineInterpolator = function (object, initialPosition) {
+  object.position.x = initialPosition.x;
+  object.position.y = initialPosition.y;
+  object.interpolated = new Interpolator(initialPosition);
+  object.interpolated.onCoordinateRequest(function () {
+    object.position.x = this.x;
+    object.position.y = this.y;
+  });
+};
 
